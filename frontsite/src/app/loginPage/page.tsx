523ba164125ae6cf6d652ddf447e4ea3bb06c8b5
Rafael Monteiro <rmxvmail@gmail.com>
2024-05-11 19:25:39 -0300

    feature/continuação do desenvolvimento da pagina de login
    	modified:   frontsite/src/app/loginPage/page.module.css
    	modified:   frontsite/src/app/loginPage/page.tsx

--- frontsite/src/app/loginPage/page.tsx
+++ frontsite/src/app/loginPage/page.tsx
@@ -1,15 +1,23 @@
+"use client"
+
 import React from 'react';
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from 'next/image';
+import styles from './page.module.css';
+import { useState } from 'react';
 
 const loginPage = () => {
+    
+    const [text, setText] = useState("");
+
     return (
         <>
             <body className={styles.window}>
                 <div className={styles.mainContainer}>
                     <img src="/images/imageDemoLogin.png" className={styles.loginImage}></img>
                     <div className={styles.subContainer}>
-                        batata
+                        <div className={styles.inputContainer} >
+                            <input type="text" onChange={(event) => setText(event.target.value)}/>
+                        </div>
                     </div>
                 </div>
             </body >
