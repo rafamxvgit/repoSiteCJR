50c9c44ca400d5b4fc7fa74506c4381028ecf506
Rafael Monteiro <rmxvmail@gmail.com>
2024-05-15 17:22:05 -0300

    feature/a tela de feed deslogado tá quase pronta
    	new file:   public/images/sad.png
    	modified:   src/app/components/searchBoxFeed.tsx
    	new file:   src/app/components/teacherIcon.tsx
    	modified:   src/app/feedNoLog/page.tsx

--- frontsite/src/app/components/searchBoxFeed.tsx
+++ frontsite/src/app/components/searchBoxFeed.tsx
@@ -1,1 +1,13 @@
 import React from "react";
+
+interface SearchBox {
+    aparencia: string
+}
+
+const SearchBoxFeed: React.FC<SearchBox> = ({aparencia}) => {
+    return( 
+        <input type="text" placeholder="Pesquisar:" className={aparencia}/>
+    )
+}
+
+export default SearchBoxFeed;
--- frontsite/src/app/feedNoLog/page.tsx
+++ frontsite/src/app/feedNoLog/page.tsx
@@ -1,22 +1,64 @@
 import React from "react";
+import SearchBoxFeed from "../components/searchBoxFeed";
+import TeacherIcon from "../components/teacherIcon";
 
 const FeedLog = () => {
 
     return(
     <>
-        <main className="w-screen h-screen flex justify-center">
-            <div className="w-5/6 h-full">
+        <main className="w-screen flex justify-center">
+            <div className="w-5/6">
                 
-                <div className="w-full h-1/6 flex justify-between">
+                <div className="w-full h-1/6 flex">
                     <div className="w-1/2 h-full ">
-                        <h1 className="h-full w-full text-3xl text-center content-around">Título:</h1>
+                        <h2 className="h-full w-full text-3xl text-center content-around">Novos Professores:</h2>
                     </div>
 
-                    <div className="w-1/2 h-full">
+                    <div className="w-1/2 h-full flex">
+                        <div className="h-1/2 w-full self-center flex justify-end">
+                            <SearchBoxFeed aparencia="w-2/3 h-full bg-lime-300 rounded-lg p-3"></SearchBoxFeed>
+                        </div>
                     </div>
                 </div>
 
-                <div className="w-full h-5/6">
+                <div className="w-full h-dvh">
+                    <section className="w-full h-1/2 flex gap-6 p-6 overflow-scroll">
+                        <TeacherIcon nome="Bach" foto="/images/sad.png"/>
+                        <TeacherIcon nome="Bach" foto="/images/sad.png"/>
+                        <TeacherIcon nome="Bach" foto="/images/sad.png"/>
+                        <TeacherIcon nome="Bach" foto="/images/sad.png"/>
+                        <TeacherIcon nome="Bach" foto="/images/sad.png"/>
+                        <TeacherIcon nome="Bach" foto="/images/sad.png"/>
+                        <TeacherIcon nome="Bach" foto="/images/sad.png"/>
+                        <TeacherIcon nome="Bach" foto="/images/sad.png"/>
+                        
+                    </section>
+                    <div className="h-0.5 w-full bg-black"></div>
+                    <section className="w-full h-dvh">
+                        <div className="w-full h-1/6">
+                            <h2 className="text-3xl w-1/2 h-full text-center content-around">Todos os professores:</h2>
+                        </div>
+                        <div className="grid grid-cols-5 h-dvh gap-4">
+                            <TeacherIcon nome="Bach" foto="/images/sad.png"/>
+                            <TeacherIcon nome="Bach" foto="/images/sad.png"/>
+                            <TeacherIcon nome="Bach" foto="/images/sad.png"/>
+                            <TeacherIcon nome="Bach" foto="/images/sad.png"/>
+                            <TeacherIcon nome="Bach" foto="/images/sad.png"/>
+                            <TeacherIcon nome="Bach" foto="/images/sad.png"/>
+                            <TeacherIcon nome="Bach" foto="/images/sad.png"/>
+                            <TeacherIcon nome="Bach" foto="/images/sad.png"/>
+                            <TeacherIcon nome="Bach" foto="/images/sad.png"/>
+                            <TeacherIcon nome="Bach" foto="/images/sad.png"/>
+                            <TeacherIcon nome="Bach" foto="/images/sad.png"/>
+                            <TeacherIcon nome="Bach" foto="/images/sad.png"/>
+                            <TeacherIcon nome="Bach" foto="/images/sad.png"/>
+                            <TeacherIcon nome="Bach" foto="/images/sad.png"/>
+                            <TeacherIcon nome="Bach" foto="/images/sad.png"/>
+                            <TeacherIcon nome="Bach" foto="/images/sad.png"/>
+                            <TeacherIcon nome="Bach" foto="/images/sad.png"/>
+                            <TeacherIcon nome="Bach" foto="/images/sad.png"/>
+                        </div>
+                    </section>
                 </div>
 
             </div>
