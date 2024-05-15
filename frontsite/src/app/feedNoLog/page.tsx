a424aa17061b94a41083296da7e374cbb49237ae
Rafael Monteiro <rmxvmail@gmail.com>
2024-05-15 17:47:20 -0300

    feature/conclusão do feed deslogado
    	modified:   frontsite/src/app/feedNoLog/page.tsx

--- frontsite/src/app/feedNoLog/page.tsx
+++ frontsite/src/app/feedNoLog/page.tsx
@@ -35,8 +35,13 @@ const FeedLog = () => {
                     </section>
                     <div className="h-0.5 w-full bg-black"></div>
                     <section className="w-full h-dvh">
-                        <div className="w-full h-1/6">
-                            <h2 className="text-3xl w-1/2 h-full text-center content-around">Todos os professores:</h2>
+                        <div className="w-full h-1/6 flex">
+                            <div className="h-full w-1/2">
+                                <h2 className="text-3xl w-full h-full text-center content-around">Todos os professores:</h2>
+                            </div>
+                            <div className="h-full w-1/2 flex justify-end">
+                                <button className="h-2/3 w-1/2 bg-lime-400 self-center text-2xl rounded-lg">Orndenar:</button>
+                            </div>
                         </div>
                         <div className="grid grid-cols-5 h-dvh gap-4">
                             <TeacherIcon nome="Bach" foto="/images/sad.png"/>
