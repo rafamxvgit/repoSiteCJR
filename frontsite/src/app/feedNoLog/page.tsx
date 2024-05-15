485f571e4f778eea78816dcace98c9759554f9af
Rafael Monteiro <rmxvmail@gmail.com>
2024-05-15 14:09:03 -0300

    feature/criação do arquivo do componente searchBoxFeed
    	new file:   src/app/components/searchBoxFeed.tsx
    	new file:   src/app/feedNoLog/page.tsx

---
+++ frontsite/src/app/feedNoLog/page.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const FeedLog = () => {
+
+    return(
+    <>
+        <main className="w-screen h-screen flex justify-center">
+            <div className="w-5/6 h-full">
+                
+                <div className="w-full h-1/6 flex justify-between">
+                    <div className="w-1/2 h-full ">
+                        <h1 className="h-full w-full text-3xl text-center content-around">Título:</h1>
+                    </div>
+
+                    <div className="w-1/2 h-full">
+                    </div>
+                </div>
+
+                <div className="w-full h-5/6">
+                </div>
+
+            </div>
+        </main>
+    </>
+    )
+}
+
+export default FeedLog;
