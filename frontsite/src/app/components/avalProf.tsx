b4ca4ad76ef3370c6e6fda28ec2384399e0d8cdd
Rafael Monteiro <rmxvmail@gmail.com>
2024-05-23 21:05:16 -0300

    acho que eu consertei parcialmente a besteira que eu tinha feito

---
+++ frontsite/src/app/components/avalProf.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Formik, Form, Field } from "formik";
+
+interface vazia {}
+
+const CampoAvaliacao: React.FC<vazia> = () => {
+    return (
+        <>
+            batata
+        </>
+    )
+}
+
+export default CampoAvaliacao;
