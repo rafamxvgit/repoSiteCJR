e7fcfde041533181e92daa89b4bf220bc4fbee6b
Rafael Monteiro <rmxvmail@gmail.com>
2024-05-26 20:25:03 -0300

    feature/ já dá pra fazer login e cadastro

--- frontsite/src/app/components/formLogin.tsx
+++ frontsite/src/app/components/formLogin.tsx
@@ -1,22 +1,29 @@
 "use client"
+import axios from "axios";
 import { Formik, Form, Field } from "formik";
-import { use } from "react";
+import { useRouter } from "next/navigation";
 import * as yup from "yup";
 
 interface boxProps {
+}
 
+interface Usuario{
+    email: string
+    senha: string
 }
 
 const FormLogin: React.FC<boxProps> = () => {
-    const initialValues = { email: "", password: "" };
+    const router = useRouter();
+    const initialValues = { email: "", senha: "" };
 
     const validation = yup.object({
         email: yup.string().email("invalid email adress").required("Required"),
-        password: yup.string().required("Required"),
+        senha: yup.string().required("Required"),
     })
 
-    const submit = (values: any) => {
-        console.log(values);
+    const submit =async (values: Usuario) => {
+        const userData = await axios.post('http://localhost:3005/user/login', values)
+        router.push(`/log/${userData.data.id}/feed`)
     }
 
     return (
@@ -25,7 +32,7 @@ const FormLogin: React.FC<boxProps> = () => {
                 <Form className="h-full w-full flex flex-col justify-evenly">
                     <h2 className="text-center text-2xl">Página de login:</h2>
                     <Field id="email" type="email" name="email" placeholder="email:" className="w-full h-10 bg-lime-400 rounded-lg p-3"></Field>
-                    <Field id="password" type="password" name="password" placeholder="password:" className="w-full h-10 bg-lime-400 rounded-lg p-3"></Field>
+                    <Field id="senha" type="password" name="senha" placeholder="senha:" className="w-full h-10 bg-lime-400 rounded-lg p-3"></Field>
                     <div className="w-full h-12 flex justify-between">
                         <button className= "h-full w-1/3 bg-lime-400 rounded-xl" type="submit">Entrar</button>
                         <a href="./register" className= "h-full w-1/3 bg-lime-400 rounded-xl text-center content-center" type="button">
--- frontsite/src/app/components/formRegister.tsx
+++ frontsite/src/app/components/formRegister.tsx
@@ -1,26 +1,29 @@
 "use client"
-
 import { Formik, Form, Field } from "formik";
 import * as yup from "yup";
 import axios from "axios";
-interface boxProps {
+import { useRouter } from "next/navigation";
 
+interface Vazia {
 }
 
-const FormRegister: React.FC<boxProps> = () => {
-    const initialValues = { email: "", password: "" };
+const FormRegister: React.FC<Vazia> = () => {
+    const router = useRouter()
+    const initialValues = { email: "", senha: ""};
 
     const validation = yup.object({
         email: yup.string().email("invalid email adress").required("Required"),
-        password: yup.string().required("Required"),
-        name: yup.string().required("Required"),
+        senha: yup.string().required("Required"),
+        nome: yup.string().required("Required"),
         curso: yup.string().required("Required"),
         departamento: yup.string().required("Required")
     })
 
     const submit = async (values: any) => {
-        const user = await axios.post('http://localhost:3003/user', values)
-        console.log(user);
+        const user = await axios.post('http://localhost:3005/user', values)
+        if (user.status == 201){
+            return router.push(`/log/${user.data.id}/feed`)
+        }
     }
 
     return (
@@ -28,9 +31,9 @@ const FormRegister: React.FC<boxProps> = () => {
             <Formik initialValues={initialValues} onSubmit={submit} validationSchema={validation}>
                 <Form className="h-full w-full flex flex-col justify-evenly">
                     <h2 className="text-center text-2xl">Registro de usuário</h2>
-                    <Field id="name" type="name" name="name" placeholder="name:" className="w-full h-10 bg-lime-400 rounded-lg p-3"></Field>
+                    <Field id="nome" type="text" name="nome" placeholder="name:" className="w-full h-10 bg-lime-400 rounded-lg p-3"></Field>
                     <Field id="email" type="email" name="email" placeholder="email:" className="w-full h-10 bg-lime-400 rounded-lg p-3"></Field>
-                    <Field id="password" type="password" name="password" placeholder="password:" className="w-full h-10 bg-lime-400 rounded-lg p-3"></Field>
+                    <Field id="senha" type="password" name="senha" placeholder="senha:" className="w-full h-10 bg-lime-400 rounded-lg p-3"></Field>
                     <Field id="curso" name="curso" placeholder="curso:" className="w-full h-10 bg-lime-400 rounded-lg p-3"></Field>
                     <Field id="departamento "type="departamento" name="departamento" placeholder="departamento:" className="w-full h-10 bg-lime-400 rounded-lg p-3"></Field>
                     <div className="w-full h-12 flex justify-center">
