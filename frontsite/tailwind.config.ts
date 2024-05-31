32f08df188dcfb6c50f87614a44c76c2b21c0c57
S3r4ph1el <cybersec.teles@gmail.com>
2024-05-31 11:32:18 -0300

    style: adicionando novas imagens e configs para o css

--- frontsite/tailwind.config.ts
+++ frontsite/tailwind.config.ts
@@ -13,6 +13,19 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        customButton: '#00ff88',
+        customBackground1: '#2f244d',
+        customBackground2: '#09618e',
+        customBackground3: '#1e6b92',
+        customText1: '#00FF7F',
+        customText2: '#40f6ff',
+        customText3: '#263238',
+        customBorder: '#455a64',
+        customWhite: '#f1f1f1',
+        customPurple: '#4B0082',
+        customGray: '#455A64',
+      }
     },
   },
   plugins: [],
