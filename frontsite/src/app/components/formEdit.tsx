e0b5d1853a314d0e5daa37f470b500c7b809693a
Rafael Monteiro <rmxvmail@gmail.com>
2024-06-05 15:00:25 -0300

    feature/ já dá editar a bio do usuário

--- frontsite/src/app/components/formEdit.tsx
+++ frontsite/src/app/components/formEdit.tsx
@@ -6,12 +6,18 @@ import * as yup from "yup";
 const initSenha = { senha: "" };
 const initFoto = { imagem: "" };
 const initNome = { nome: "" };
+const initBio = { bio: "" };
+
 const validationSenha = yup.object({
   senha: yup.string().required("Required"),
 });
 const validationFoto = yup.object({
   imagem: yup.string().required("Required"),
 });
+const validationBio = yup.object({
+  bio: yup.string().required("Required"),
+});
+
 const validationNome = yup.object({ nome: yup.string().required("Required") });
 
 interface Valores {
@@ -33,6 +39,11 @@ const FormEdit: React.FC<Valores> = ({ userID }) => {
     await axios.patch(`http://localhost:3005/user/nome${userID}`, values);
     window.location.reload();
   };
+
+  const submitBio = async (values: { bio: string }) => {
+    await axios.patch(`http://localhost:3005/user/bio${userID}`, values)
+    window.location.reload();
+  }
 
   return (
     <>
@@ -81,6 +92,34 @@ const FormEdit: React.FC<Valores> = ({ userID }) => {
                   type="text"
                   name="imagem"
                   placeholder="Mudar Foto"
+                  className="w-full h-12 bg-lime-300 rounded-md p-6 outline-none border border-black focus:border-2 focus:border-black focus:shadow focus:shadow-black"
+                />
+              </div>
+              <div className="w-2/6">
+                <button
+                  className="h-12 w-full ml-3 bg-lime-300 rounded-md border border-black hover:border-2 hover:border-black hover:shadow hover:shadow-black hover:bg-lime-400"
+                  type="submit"
+                >
+                  Enviar
+                </button>
+              </div>
+            </Form>
+          </Formik>
+        </div>
+        
+        <div className="flex flex-col w-full my-6">
+          <Formik
+            initialValues={initBio}
+            validationSchema={validationBio}
+            onSubmit={submitBio}
+          >
+            <Form className="w-full flex justify-center">
+              <div className="w-5/6">
+                <Field
+                  id="bio"
+                  type="text"
+                  name="bio"
+                  placeholder="Mudar Bio"
                   className="w-full h-12 bg-lime-300 rounded-md p-6 outline-none border border-black focus:border-2 focus:border-black focus:shadow focus:shadow-black"
                 />
               </div>
