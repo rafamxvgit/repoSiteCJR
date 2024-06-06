c321d20c32e7ac7f80ef418f6f5b0e1afd5f55cf
Rafael Monteiro <rmxvmail@gmail.com>
2024-06-06 12:58:19 -0300

    feature/agora o ícone nos comentários dos usuários é um link para seus respectivos perfis

--- frontsite/src/app/components/comentario.tsx
+++ frontsite/src/app/components/comentario.tsx
@@ -6,10 +6,18 @@ interface CometnarioProps { comentario: ComentarioDTO, loged: number }
 const Comentario: React.FC<CometnarioProps> = ({ comentario, loged }) => {
 
   let BotaoExcluir: () => JSX.Element;
+  let theLink: string;
 
   const removerComentario = async () => {
     await axios.delete(`http://localhost:3005/comentario/${comentario.id+''}`)
-    window.location.reload()
+    window.location.reload();
+  }
+
+  if (loged != -1) {
+    theLink = `/log/${loged}/perfil/${comentario.idAutor}`
+  }
+  else {
+    theLink = `/noLog/perfil/${comentario.idAutor}`
   }
 
   if (comentario.idAutor == loged) {
@@ -30,10 +38,10 @@ const Comentario: React.FC<CometnarioProps> = ({ comentario, loged }) => {
   return (
     <div className="flex flex-col gap-2">
       <div className="h-1 bg-black my-3"></div>
-      <div className="flex gap-2">
+      <a className="flex gap-2" href={theLink}>
         <img className="h-10 aspect-square rounded-full" src={comentario.foto} alt="" />
         <p className="self-center">{comentario.nomeAutor.toString()} comentou:</p>
-      </div>
+      </a>
       <div className="flex">
         <p className="bg-blue-200 w-full rounded-lg p-2">{comentario.conteudo}</p>
       </div>
